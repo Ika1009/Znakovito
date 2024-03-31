5078d87e9e146bd45c001bc6a458fe70921bf8e7
Ilija Dončić <75073537+Ika1009@users.noreply.github.com>
2024-03-31 03:45:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -44,107 +44,28 @@ const handleAudioFile = async (input) => {
         displaySignLanguage(text);
     }
 };
-
-function displaySignLanguage(text) {
+function displaySignLanguage(text, languageSelectedCurrent) {
     // Clear previous images
     clearSignLanguage();
     // Assuming 'text' is the input text
     const container = document.getElementById('sign-language-container');
-    text = replaceLovePhrases(text);
 
-    // Map English special words to their Serbian equivalents
-    const specialWordsMapping = {
-        'mother': 'majk',
-        'madre': 'majk',
-        'friend': 'prijatelj',
-        'amigo': 'prijatelj',
-        'hello': 'zdravo',
-        'halo': 'zdravo',
-        'thank you': 'hvala',
-        'please': 'molim',
-    };
-        
-    // Loop through each word in the input text
-    text.split(' ').forEach(word => {
-        if(word == "ily")
-        {
-            showImage("ily");
-            continue;
-        }
-        
-        
-        // Check if the current word is a special word based on the selected language
-        let specialWord = '';
-        
-        if (languageSelectedCurrent === 'sr') {
-            specialWord = ['majk', 'prijatelj', 'zdravo'].includes(word.toLowerCase()) ? word.toLowerCase() : '';
-        } else if (languageSelectedCurrent === 'es') {
-            specialWord = ['madre', 'amigo', 'hola'].includes(word.toLowerCase()) ? word.toLowerCase() : '';
-        } else if (languageSelectedCurrent === 'en') {
-            specialWord = ['mother', 'friend', 'hello'].includes(word.toLowerCase()) ? word.toLowerCase() : '';
-        }
-        if(specialWord && specialWord !== '') {
-            showImage(specialWord);
-        }
-        
-        // Loop through each letter in the word
-        for (let i = 0; i < word.length; i++) {
-            let letter = word[i].toLowerCase(); // Assuming your image filenames are lowercase
-            
-            // Check if the character is from Cyrillic script and the language selected is Serbian
-            if (languageSelectedCurrent === 'sr' && /[\u0400-\u04FF]/.test(letter)) { // Range for Cyrillic characters
-                // Convert Cyrillic to Latin script if applicable
-                letter = convertToLatin(letter);
-            }
-            
-            // Check for combinations 'lj', 'nj', 'dž', and 'đ' only if the language selected is Serbian
-            if (languageSelectedCurrent === 'sr') {
-                const nextLetter = i < word.length - 1 ? word[i + 1].toLowerCase() : '';
-                if ((letter === 'l' || letter === 'n') && nextLetter === 'j') {
-                    // Combine 'lj' or 'nj' into a single character
-                    letter += 'j';
-                    // Increment the loop counter to skip the next letter
-                    i++;
-                } else if (letter === 'd' && nextLetter === 'ž') {
-                    // Combine 'd' and 'ž' into 'dž'
-                    letter = 'dž';
-                    // Increment the loop counter to skip the next letter
-                    i++;
-                } else if (letter === 'd' && nextLetter === 'j') {
-                    // Combine 'd' and 'j' into 'đ'
-                    letter = 'đ';
-                    // Increment the loop counter to skip the next letter
-                    i++;
-                }
-            }
-            showImage(letter);
-        }
-        
-        // Add a space between words
-        const space = document.createElement('div');
-        space.width = 500;
-        space.height = 5;
-        container.appendChild(space);
-    });
-
-    function showImage(letter){       
-        // Create an image element
+    // Function to show an image for a letter
+    function showImage(letter) {
         const img = document.createElement('img');
         img.width = 100;
         img.height = 100;
-        // img.style.objectFit = "contain";
-        img.src = `./znakovi/${letter}.png`; // Assuming the images are stored in the 'znakovi' directory
-        img.alt = letter; // Set alt text for accessibility 
-                    // Append the image to the container
+        img.src = `./znakovi/${letter}.png`;
+        img.alt = letter;
         container.appendChild(img);
     }
 
-    const lovePhrases = {
-        'sr': ['volim te', 'ljubim te', 'volem te', 'volim vas', 'ljubim vas', 'volem vas'],
-        'en': ['i love you', 'love you', 'luv ya', 'luv u', 'love ya', 'love u']
-    };
-    
+    // Function to check and replace love phrases
     const replaceLovePhrases = (input) => {
+        const lovePhrases = {
+            'sr': ['volim te', 'ljubim te', 'volem te', 'volim vas', 'ljubim vas', 'volem vas'],
+            'en': ['i love you', 'love you', 'luv ya', 'luv u', 'love ya', 'love u']
+        };
         let replacedInput = input;
         for (const lang in lovePhrases) {
             if (lovePhrases.hasOwnProperty(lang)) {
@@ -158,20 +79,76 @@ function displaySignLanguage(text) {
         return replacedInput;
     };
 
+    // Function to map special words based on the selected language
+    const mapSpecialWords = (word, languageSelectedCurrent) => {
+        const specialWordsMapping = {
+            'sr': ['majk', 'prijatelj', 'zdravo'],
+            'es': ['madre', 'amigo', 'hola'],
+            'en': ['mother', 'friend', 'hello']
+        };
+        const specialWords = specialWordsMapping[languageSelectedCurrent] || [];
+        const specialWord = specialWords.includes(word.toLowerCase()) ? word.toLowerCase() : '';
+        if (specialWord && specialWord !== '') {
+            showImage(specialWord);
+            return true; // Indicate that a special word was found
+        }
+        return false; // Indicate that no special word was found
+    };
+
+    // Replace love phrases with 'ily'
+    text = replaceLovePhrases(text);
+
+    // Loop through each word in the input text
+    text.split(' ').forEach(word => {
+        // Check for special cases
+        if (word === "ily") {
+            showImage("ily");
+            return;
+        }
+        if (mapSpecialWords(word, languageSelectedCurrent)) {
+            return; // Move to the next iteration if a special word is found
+        }
+        // Loop through each letter in the word
+        for (let i = 0; i < word.length; i++) {
+            let letter = word[i].toLowerCase();
+            // Check if the character is from Cyrillic script and the language selected is Serbian
+            if (languageSelectedCurrent === 'sr' && /[\u0400-\u04FF]/.test(letter)) {
+                letter = convertToLatin(letter);
+            }
+            // Check for combinations 'lj', 'nj', 'dž', and 'đ' only if the language selected is Serbian
+            if (languageSelectedCurrent === 'sr') {
+                const nextLetter = i < word.length - 1 ? word[i + 1].toLowerCase() : '';
+                if ((letter === 'l' || letter === 'n') && nextLetter === 'j') {
+                    letter += 'j';
+                    i++;
+                } else if (letter === 'd' && nextLetter === 'ž') {
+                    letter = 'dž';
+                    i++;
+                } else if (letter === 'd' && nextLetter === 'j') {
+                    letter = 'đ';
+                    i++;
+                }
+            }
+            showImage(letter);
+        }
+        // Add a space between words
+        const space = document.createElement('div');
+        space.width = 500;
+        space.height = 5;
+        container.appendChild(space);
+    });
+
     // Function to convert Cyrillic to Latin script
     function convertToLatin(char) {
-        // Define a mapping between Cyrillic and Latin characters
         const cyrillicToLatinMap = {
             'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'ђ': 'đ', 'е': 'e', 'ж': 'ž', 'з': 'z', 'и': 'i',
             'ј': 'j', 'к': 'k', 'л': 'l', 'љ': 'lj', 'м': 'm', 'н': 'n', 'њ': 'nj', 'о': 'o', 'п': 'p', 'р': 'r',
             'с': 's', 'т': 't', 'ћ': 'ć', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'č', 'џ': 'dž', 'ш': 'š'
         };
-        
-        // If the character exists in the mapping, return its Latin counterpart, otherwise return the original character
         return cyrillicToLatinMap[char] || char;
     }
-
 }
+
 
 function clearSignLanguage() {
     const container = document.getElementById('sign-language-container');
