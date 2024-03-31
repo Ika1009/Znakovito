af1e5334eb283303252fd708a618098f480a3152
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-03-31 09:31:38 +0200

    .

--- landing.js
+++ landing.js
@@ -4,51 +4,42 @@ const selectLanguage = (langCode) => {
     switch(langCode) {
       case 'en':
         selectedLanguageCurrent = "en";    
-        selectedLanguage.textContent = "English";
-        inputFileText.textContent = "Input File";
-        uploadFileText.textContent = "Upload or drag & drop your file.";
-        textInput.placeholder = "Type here";
-        textInputButton.textContent = "Submit";
+        languageButton.textContent = "English";
+        let img = document.createElement('img');
+        img.src = 'https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg';
+        img.alt = 'Description of the image';
+        img.classList.add('h-5', 'w-5', 'rounded-full', 'me-2');
+        let targetDiv = document.getElementById('languageButton');
+        targetDiv.appendChild(img);
+        heder1.textContent = "Practice";
+        heder2.textContent = "Quiz";
+        heder3.textContent = "Translation";
+        tekst1.textContent = "Your guide through the worlds sign languages!";
+        tekst2.textContent = "Different countries, different sign languages. With Znakovito, overcome language barriers and learn sign language from any country.";
+        tekst3.textContent = "Get started";
+        tekst4.textContent = "Deaf and mute persons";
+        tekst5.textContent = "marries another deaf and mute person";
+        tekst6.textContent = "Audio and text translation";
+        tekst7.textContent = "Whether you use voice messages, text input or audio files, Znakovito offers accurate sign language translation - adapted to different local dialects.";
+        tekst8.textContent = "Quiz for two";
+        tekst9.textContent = "Feel the connection with your partner, friend or family member through a challenging quiz, where your skills in learning sign language are scored.";
+        tekst10.textContent = "Learning through games";
+        tekst11.textContent = "Explore our single-player quiz that allows you to quickly learn new sign languages ​​while traveling abroad or preparing to communicate in new locations.";
         break;
       case 'fr':
         selectedLanguageCurrent = "fr";
-        selectedLanguage.textContent = "Francuski";
-        inputFileText.textContent = "Fichier d'entrée";
-        uploadFileText.textContent = "Téléchargez ou glissez-déposez votre fichier.";
-        textInput.placeholder = "Écrivez ici";
-        textInputButton.textContent = "Soumettre";
         break;
       case 'sr':
         selectedLanguageCurrent = "sr";    
-        selectedLanguage.textContent = "Srpski";
-        inputFileText.textContent = "Улазни документ";
-        uploadFileText.textContent = "Отпремите или превуците и отпустите своју датотеку";
-        textInput.placeholder = "Откуцајте овде";
-        textInputButton.textContent = "Потврди";
         break;
       case 'de':
         selectedLanguageCurrent = "de";
-        selectedLanguage.textContent = "Nemački";
-        inputFileText.textContent = "Eingabedatei";
-        uploadFileText.textContent = "Laden Sie Ihre Datei hoch oder ziehen Sie sie per Drag & Drop";
-        textInput.placeholder = "Geben Sie hier ein";
-        textInputButton.textContent = "Einreichen";
         break;
       case 'it':
-        selectedLanguageCurrent = "it";    
-        selectedLanguage.textContent = "Italijanski";
-        inputFileText.textContent = "File di input";
-        uploadFileText.textContent = "Carica o trascina e rilascia il tuo file";
-        textInput.placeholder = "Digitare qui";
-        textInputButton.textContent = "Invia";
+        selectedLanguageCurrent = "it";  
         break;
       case 'es':
-        selectedLanguageCurrent = "es";    
-        selectedLanguage.textContent = "Španski";
-        inputFileText.textContent = "Entrada de archivo";
-        uploadFileText.textContent = "Sube o arrastra y suelta tu archivo";
-        textInput.placeholder = "Escriba aquí";
-        textInputButton.textContent = "Entregar";
+        selectedLanguageCurrent = "es";   
         break;
       default:
         selectedLanguage.textContent = "Select Language";
