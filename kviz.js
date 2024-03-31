82baf9fcaf8295aad9de887d464ea07891ca8770
Ilija Dončić <75073537+Ika1009@users.noreply.github.com>
2024-03-31 10:34:14 +0200

    Update kviz.js

--- kviz.js
+++ kviz.js
@@ -118,28 +118,49 @@ const quizQuestionsNaEngleskom = [
 let currentQuestionIndex = 0;
 
 function displayQuestion() {
+    console.log("Inside displayQuestion function");
+
     const questionContainer = document.getElementById("question");
+    console.log("questionContainer:", questionContainer);
+
     const optionsContainer = document.getElementById("options");
+    console.log("optionsContainer:", optionsContainer);
 
     const currentQuestion = quizQuestionsNaEngleskom[currentQuestionIndex];
+    console.log("currentQuestion:", currentQuestion);
+
     questionContainer.textContent = currentQuestion.question;
+    console.log("Question set:", currentQuestion.question);
+
     let imgCounter = 1;
-    // Get the image element by its id
-    const imageElement = document.getElementById('1');
-    console.log(imageElement);
+
     currentQuestion.options.forEach((option, index) => {
-        const img = document.getElementById(imgCounter);
-        img.src = option.text;
-    
-        const grandparent = img.parentElement.parentElement;
-        const newGrandparent = grandparent.cloneNode(true);
-        img.parentElement.replaceChild(newGrandparent, grandparent);
-        newGrandparent.addEventListener("click", () => checkAnswer(option.isCorrect));
+        console.log("Inside forEach loop for options");
         
+        const img = document.getElementById(imgCounter);
+        console.log("img:", img);
+
+        img.src = option.text;
+        console.log("Image source set:", option.text);
+
+        const grandparent = img.parentElement.parentElement;
+        console.log("grandparent:", grandparent);
+
+        const newGrandparent = grandparent.cloneNode(true);
+        console.log("newGrandparent:", newGrandparent);
+
+        img.parentElement.replaceChild(newGrandparent, grandparent);
+        console.log("Parent replaced with new clone");
+
+        newGrandparent.addEventListener("click", () => checkAnswer(option.isCorrect));
+        console.log("Event listener added for option:", option.isCorrect);
+
         imgCounter++;
     });
 
+    console.log("Exiting displayQuestion function");
 }
+
 
 function checkAnswer(isCorrect) {
     console.log("Check answer")
